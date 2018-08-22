22b5153042424bebb40e15620befc8cf759db95a
olehcambel <nickcambel5@gmail.com>
2018-08-22 04:52:23 +0200

    add ApolloProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import CommentList from './components/CommentList';
+
+// client setup for apollo
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
 
 class App extends Component {
   render() {
-    return <div>Get started with React</div>;
+    return (
+      <ApolloProvider client={client}>
+        <h2>Main page</h2>
+        <CommentList />
+      </ApolloProvider>
+    );
   }
 }
 
---
+++ client/src/components/CommentList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CommentList extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li> Comment name </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default CommentList;
